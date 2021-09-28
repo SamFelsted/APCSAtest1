e94e405bade205f68b115ca50cf89a9971e33ceb
SamFelsted <sam.felsted@icloud.com>
2021-09-28 09:36:59 -0700

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,16 +11,29 @@ public class Main {
         int num2 = safeInput(read);
         System.out.println("The sum of the two numbers is " + (num1 + num2));
         System.out.println("The difference of the two numbers is " + (num1 - num2));
+        System.out.println("The product of the two numbers is " + (num1 * num2));
+        System.out.println("The average of the two numbers is " + ((double) num1 + num2) / 2);
+        System.out.println("The distance between the two is " + Math.abs(num1 - num2));
+        int max, min;
+        if (num1 > num2){
+            max = num1;
+            min = num2;
+        } else {
+            min = num1;
+            max = num2;
+        }
+        System.out.println("The min is " + min);
+        System.out.println("The max is " + max);
 
     }
 
     public static int safeInput(Scanner read) {
         while (true) {
             try {
-                System.out.println("Hello, please type in an integer");
+                System.out.println("please type in an integer");
                 return read.nextInt();
             } catch (Exception e) {
-                System.out.println("Error, not int");
+                System.out.print("Error, not an integer, ");
                 read.nextLine();
             }
         }
